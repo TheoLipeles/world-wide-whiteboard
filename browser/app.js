6ef9593089c67193ba81dbd7a591f47f3130777f
Theo Lipeles <Theo@Lipeles.com>
2015-07-08 14:53:05 -0400

    not much

---
+++ browser/app.js
@@ -0,0 +1,17 @@
+
+
+
+
+window.whiteboard.on("draw", function(start, end, strokeColor) {
+	console.log('start', start);
+	console.log('end', end);
+	console.log('strokeColor', strokeColor);
+});
+
+
+
+
+
+
+
+
