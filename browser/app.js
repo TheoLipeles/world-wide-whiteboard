50cb70c9103ca25e8a15456631541d5de3d57fc4
Theo Lipeles <Theo@Lipeles.com>
2015-07-08 16:00:55 -0400

    done with tasks

--- browser/app.js
+++ browser/app.js
@@ -1,17 +1,20 @@
 
-
+var socket = io(window.location.origin);
 
 
 window.whiteboard.on("draw", function(start, end, strokeColor) {
-	console.log('start', start);
-	console.log('end', end);
-	console.log('strokeColor', strokeColor);
+	socket.emit("draw", start, end, strokeColor);
 });
 
 
+socket.on("connect", function() {
+	console.log("It happened");
+});
 
+socket.on('disconnect', function() {
+	console.log("don't go!");
+});
 
-
-
-
-
+socket.on('draw', function(start, end, strokeColor) {
+	window.whiteboard.draw(start, end, strokeColor);
+});
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var path = require('path');
-
+var socketio = require('socket.io');
 var http = require('http');
 var server = http.createServer();
 
@@ -7,6 +7,18 @@ var express = require('express');
 var app = express();
 
 server.on('request', app);
+
+var io = socketio.listen(server);
+
+io.on('connection', function(socket) {
+	console.log("A new client has connected");
+	console.log(socket.id);
+
+
+	socket.on("draw", function(start, end, strokeColor) {
+		socket.broadcast.emit("draw", start, end, strokeColor);
+	});
+});
 
 server.listen(1337, function () {
     console.log('The server is listening on port 1337!');
