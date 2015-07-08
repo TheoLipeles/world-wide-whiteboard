e9612619820d55cf1deb95a4ce19f35b5e1b3841
Christian Evans <christian.evans214@gmail.com>
2015-07-08 16:42:49 -0400

    persisted drawing

--- browser/app.js
+++ browser/app.js
@@ -1,4 +1,3 @@
-
 var socket = io(window.location.origin);
 
 
@@ -10,6 +9,13 @@ window.whiteboard.on("draw", function(start, end, strokeColor) {
 socket.on("connect", function() {
 	console.log("It happened");
 });
+
+socket.on('beginningdraw', function(dataArr) {
+	console.log(dataArr);
+	dataArr.forEach(function(data, index) {
+		window.whiteboard.draw.apply(null, data);
+	})
+})
 
 socket.on('disconnect', function() {
 	console.log("don't go!");
--- server.js
+++ server.js
@@ -2,30 +2,30 @@ var path = require('path');
 var socketio = require('socket.io');
 var http = require('http');
 var server = http.createServer();
-
 var express = require('express');
 var app = express();
-
 server.on('request', app);
+
+var dataArr = [];
 
 var io = socketio.listen(server);
 
 io.on('connection', function(socket) {
 	console.log("A new client has connected");
 	console.log(socket.id);
-
-
+	socket.emit('beginningdraw', dataArr);
 	socket.on("draw", function(start, end, strokeColor) {
+		dataArr.push([start, end, strokeColor]);
 		socket.broadcast.emit("draw", start, end, strokeColor);
 	});
 });
 
-server.listen(1337, function () {
-    console.log('The server is listening on port 1337!');
+server.listen(1337, function() {
+	console.log('The server is listening on port 1337!');
 });
 
 app.use(express.static(path.join(__dirname, 'browser')));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
